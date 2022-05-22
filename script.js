2a57d9d1b17160b0be6f0fde31550509a1d6e5e5
jardean <jardeanalef@gmail.com>
2022-05-22 13:22:46 -0300

    adicionando novas funcionalidades na validação do cpf

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function validaCPF(cpf) {
     } else {
         var numeros = cpf.substring(0,9);
         var digitos = cpf.substring(9);
-
-        console.log("numeros o cpf " + numeros);
-        console.log("digito do cpf " + digitos);
+    }
+        //console.log("numeros o cpf " + numeros);
+        //console.log("digito do cpf " + digitos);
 
         var soma = 0;
         for (var i = 10; i > 1; i--) {
@@ -17,14 +17,34 @@ function validaCPF(cpf) {
         }
         console.log(soma);
 
-        var resultado = soma % 11 < 2
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+        //validação do Primeiro Digito
+        if (resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+            }
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+             //validação do segundo digito
+            if (resultado != digitos.charAt(1)) {
+                return false;
+            }
 
         return true;
     }
-}
 
 function validacao() {
     console.log("Iniciando validação CPF");
+    document.getElementById("success").style.display = "none";
+    document.getElementById("error").style.display = "none";
+
 
     var cpf = document.getElementById("cpf_digitado").value;
 
